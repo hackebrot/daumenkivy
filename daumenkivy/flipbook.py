391b171908253c61842e80e2b323098358c32b88
Raphael Pierzina <raphael@hackebrot.de>
2014-05-11 00:57:42 +0200

    Add name property to FlipBook

--- daumenkivy/flipbook.py
+++ daumenkivy/flipbook.py
@@ -1,7 +1,8 @@
 from kivy.uix.carousel import Carousel
+from kivy.properties import StringProperty
 
 
 class FlipBook(Carousel):
     """Carousel representing the timeline of the animation.
     """
-    pass
+    name = StringProperty()
--- tests/test_flipbook.py
+++ tests/test_flipbook.py
@@ -4,6 +4,15 @@
 from daumenkivy.flipbook import FlipBook
 
 
-def test_flip_book():
-        FlipBook()
+class TestFlipBook(object):
+    """TestCase for the FlipBook carousel.
+    """
+    def setup(self):
+        self.fb = FlipBook()
+
+    def test_name(self):
+        assert self.fb.name == ''
+
+    def teardown(self):
+        pass
 
