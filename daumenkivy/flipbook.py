1fb438458b482be100bb0715d80c5a74c69754f6
Raphael Pierzina <raphael@hackebrot.de>
2014-05-11 00:41:42 +0200

    Create FlipBook class

---
+++ daumenkivy/flipbook.py
@@ -0,0 +1,7 @@
+from kivy.uix.carousel import Carousel
+
+
+class FlipBook(Carousel):
+    """Carousel representing the timeline of the animation.
+    """
+    pass
---
+++ tests/test_flipbook.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from daumenkivy.flipbook import FlipBook
+
+
+def test_flip_book():
+        FlipBook()
+
