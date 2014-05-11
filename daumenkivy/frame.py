c88ddfb6f52637bee1ebe4149a9602eab7fcea98
Raphael Pierzina <raphael@hackebrot.de>
2014-05-11 13:43:01 +0200

    Create class representing a single frame of the flip-book

---
+++ daumenkivy/frame.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from kivy.uix.floatlayout import FloatLayout
+
+
+class Frame(FloatLayout):
+    """A single frame of the flip-book animation.
+    """
+    pass
---
+++ tests/test_frame.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from daumenkivy.frame import Frame
+
+
+def test_frame():
+    Frame()
