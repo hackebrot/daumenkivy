ecbe3f90a0b0837e529a5a5c7439431753011bbb
Raphael Pierzina <raphael@hackebrot.de>
2014-05-18 18:46:23 +0200

    Create FlipBookManager class

---
+++ daumenkivy/flipbookmanager.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from kivy.uix.screenmanager import ScreenManager
+
+
+class FlipBookManager(ScreenManager):
+    """Custom ScreenManager providing callbacks for switching FlipBooks.
+    """
+
